c4d41fe3ba1ce75e23222775e41605fa8f54e2a7
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2023-04-02 10:49:26 +0530

    download icon added

--- youtility-web-app/src/components/media-card/index.tsx
+++ youtility-web-app/src/components/media-card/index.tsx
@@ -1,7 +1,9 @@
 import { YoutubeVideoDetail } from "@/types/youtube"
 import Dropdown from "../dropdown"
 import Image from "next/image"
-import { limit } from "@/lib/utils"
+import { limit } from "@/lib/utils";
+
+import { BsDownload } from 'react-icons/bs';
 
 
 interface Props extends YoutubeVideoDetail { }
@@ -16,12 +18,16 @@ function MediaCard({ videoId, title, duration, thumbnail, definition }: Props) {
                 <div className="text-lg whitespace-nowrap md:tracking-wide" title={title}>{limit(title, 50)}</div>
                 <div className="text-md text-xs text-gray-500 mt-1">{duration}</div>
                 <div className="flex mt-4 justify-between">
-                    <button className="border rounded md:p-3 p-1 shadow bg-black text-white md:text-md text-sm">Download</button>
                     <Dropdown
                         options={[{ label: "MP4 720P", value: "mp4-720" }, { label: "MP4 360P", value: "mp4-360" }]}
                         value={{ label: "MP4 720P", value: "mp4-720" }}
                         onChange={(option) => console.log(option)}
                     />
+                    <button 
+                        title="Download"
+                        className="border rounded md:p-3 p-1 shadow bg-black text-white text-lg">
+                            <BsDownload />
+                    </button>
                 </div>
             </div>
         </div>
