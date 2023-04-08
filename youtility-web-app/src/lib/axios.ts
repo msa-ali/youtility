477a7d858e8a1bb02df59847f6f720335e5486ff
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2023-04-08 05:48:07 +0530

    updating base url

--- youtility-web-app/src/lib/axios.ts
+++ youtility-web-app/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.BASE_URL || "http://localhost:8080";
+export const BASE_URL = process.env.BASE_URL || "https://youtility-app.azurewebsites.net";
 
 axios.defaults.baseURL = BASE_URL;
 
